d8a396ceb36c98417f8961d2c35fdb5486dd5702
nlamprea <nlamprea@unal.edu.co>
2022-05-24 19:12:08 -0500

    g

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -46,12 +46,12 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="text-center mt-5">
+                        <div class="text-center mt-5" >
                             <h3>Jessica Jones
                                 <span class="font-weight-light">, 27</span>
                             </h3>
-                            <div class="h6 font-weight-300"><i class="ni location_pin mr-2"></i>Bucharest, Romania</div>
-                            <div class="h6 mt-4"><i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer</div>
+                            <div class="h6 font-weight-300"><i class="ni location_pin mr-2"></i>Nombre: {{getVolunteerInformation.setPost}} </div>
+                            <div class="h6 mt-4"><i class="ni business_briefcase-24 mr-2"></i>name</div>
                             <div><i class="ni education_hat mr-2"></i>University of Computer Science</div>
                         </div>
                         <div class="mt-5 py-5 border-top text-center">
@@ -59,6 +59,13 @@
                                 <div class="col-lg-9">
                                     <p>An artist of considerable range, Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. An artist of considerable range.</p>
                                     <a href="#">Show more</a>
+                                    <p>holaname</p>
+                                    <tr v-on:submit.prevent="getVolunteerInformation">
+                                        <th scope="row">1</th>
+                                        <td>session.name</td>
+                                        <td>getVolunteerInformation.name</td>
+                                    </tr>
+
                                 </div>
                             </div>
                         </div>
@@ -69,7 +76,73 @@
     </div>
 </template>
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            email: "",
+            username: "",
+            Voluntarioname:"",
+            document: "",
+            phone: "",
+            birthday: ""
+            /*uservoluntar : true,*/
+        };
+    },
+    methods: {
+        async getVolunteerInformation(){
+            
+            try {
+                var datavoluntario = {
+                    email: this.email,
+                    username: this.username,
+                    Voluntarioname: this.name,
+                    document: this.document,
+                    phone: this.phone,
+                    birthday: this.birthday,
+                };
+
+                let response = await axios.get("http://localhost:8000/api/auth/volunteer", {
+                    headers:{
+                        'Authorization': localStorage.getItem('token')
+                    }
+                
+                });
+             this.dataVoluntario = response.data;
+            //commit('setPosts', response.data)
+         } catch (error) {
+             console.log(error.response.data)
+           }
+     },
+     
+    }
+    
+};
+/*export default {
+    data(){
+        return{
+            record:'',
+            loading:true,
+            error:false,
+        }
+    },
+
+    mounted(){
+        
+        axios.get("http://localhost:8000/api/auth/volunteer")
+        .then(response =>{
+            this.record=response.data.data[0]
+        })
+        .catch(error=>{
+            console.log(error)
+            this.error=true
+        })
+        .finally(()=>{
+            this.loading=false
+        })
+    },
+}*/
 </script>
 <style>
 </style>
