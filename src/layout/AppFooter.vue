47f05918b55360c0718268b52601995506576cb8
Johan15111 <jolemusp@unal.edu.co>
2022-06-30 11:13:05 -0500

    Added to footer FAQ and legal

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -12,18 +12,13 @@
                 <div class="col-md-6">
                     <ul class="nav nav-footer justify-content-end">
                         <li class="nav-item">
-                            <a href="https://www.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Creative Tim</a>
+                            <a href="#/FAQ" class="nav-link" rel="noopener">Preguntas Frecuentes</a>
                         </li>
                         <li class="nav-item">
-                            <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank" rel="noopener">About
-                                Us</a>
+                            <a href="#/legal" class="nav-link" rel="noopener">Legal</a>
                         </li>
                         <li class="nav-item">
-                            <a href="http://blog.creative-tim.com" class="nav-link" target="_blank" rel="noopener">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md"
-                               class="nav-link" target="_blank" rel="noopener">MIT License</a>
+                            <a href="#/" class="nav-link" target="_blank" rel="noopener">Blog</a>
                         </li>
                     </ul>
                 </div>
